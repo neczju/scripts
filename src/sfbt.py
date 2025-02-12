1d98da5f5da7605e5f37eb7e1ef24bd6cc9449e9
neczju <34449700+neczju@users.noreply.github.com>
2025-02-12 17:54:27 +0100

    updated sfbt.py

--- src/sfbt.py
+++ src/sfbt.py
@@ -7,28 +7,39 @@ import os
 import sys
 import shutil
 
+
 savefile_path = {
-        'vs': '.config/VintagestoryData/Saves/',
-        'kenshi': ('.steam/debian-installation/steamapps/compatdata/233860'
-                   '/pfx/drive_c/users/steamuser/AppData/Local/kenshi/'),
-        'nier': ('.steam/debian-installation/steamapps/compatdata/524220/pfx'
-                 '/drive_c/user/steamuser/My Documents/My Games/NieR_Automata/'),
+        'vintagestory': '.config/VintagestoryData/Saves/',
+        'kenshi': '233860/pfx/drive_c/users/steamuser/AppData/Local/kenshi/',
+        'nier': ('524220/pfx/drive_c/user/steamuser/My Documents/'
+                 'My Games/NieR_Automata/'),
         'minecraft': ('.var/app/org.prismlauncher.PrismLauncher/data'
                       '/PrismLauncher/instances/1.21.4/minecraft/saves'),
+        'skyrim': ('489830/pfx/drive_c/users/steamuser/Documents/My Games/'
+                   'Skyrim Special Edition/Saves'),
         }
+
+steam_games = ['nier', 'kenshi', 'skyrim']
+steam_compatdata_path = Path(
+        '.steam/debian-installation/steamapps/compatdata/')
 home_path = Path.home()
 target_path = 'backups'
 
 if len(sys.argv) < 2:
     print('Type an argument!')
+    available_arguments = ', '.join(savefile_path.keys())
+    print("available arguments: %s" % available_arguments)
     sys.exit()
 
 argument = sys.argv[1]
+backup_filename = argument
 
 # sets source_dir and backup_filename if argument is in dictionary
-if argument in savefile_path:
+if argument in steam_games:
+    source_path = Path(home_path / steam_compatdata_path
+                       / savefile_path[argument])
+elif argument in savefile_path:
     source_path = Path(home_path / savefile_path[argument])
-    backup_filename = argument
 else:
     print('No argument named ' + argument + '!')
     sys.exit()
@@ -40,7 +51,7 @@ if not Path(target_path).exists():
 # checks if source direcotry exist and creates backup with current localtime
 if Path(source_path).exists():
     localtime_format = time.strftime('_%d_%m_%Y_%H_%M_%S', time.localtime())
-    os.chdir(target_path) 
+    os.chdir(target_path)
     shutil.make_archive(backup_filename + localtime_format, 'zip', source_path)
     print(f"{argument} backup successful!")
 else:
